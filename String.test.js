cd4a7713e12ea190969a022f6b618fe0d0c78092
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-14 20:53:41 -0600

    Create test for know the length of a string

--- String.test.js
+++ String.test.js
@@ -2,18 +2,35 @@ const stringLength = require('./String');
 
 // Know the length of a string
 it('String length 1', () => {
-    expect(stringLength('Hello')).toBe(5);
+    //Arrange
+    const string = 'Hello';
+
+    //Act
+    const length = stringLength(string);
+
+    //Assert
+    expect(length).toBe(5);
 })
 
 it('String length 2', () => {
-    expect(stringLength('Cat')).toBe(3);
+    const string = 'Cat';
+
+    const length = stringLength(string);
+
+    expect(length).toBe(3);
 })
 
 it('String length 3', () => {
-    expect(stringLength('Potato')).toBe(6);
+    const string = 'Potato';
+
+    const length = stringLength(string);
+
+    expect(length).toBe(6);
 })
 
 // Return an error when the length is lees than 1 and more than 10
 it('Not string length conditions approved', () => {
-    expect(() => stringLength('Hello Cats')).toThrow();
+    const string = 'Hello Cats';
+    
+    expect(() => stringLength(string)).toThrow();
 })
