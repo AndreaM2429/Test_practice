9efb3c245ff26de4ce5fa46af2a1c36b528d5431
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-14 20:54:25 -0600

    Create test for basic methods of a calculator

--- calculator.test.js
+++ calculator.test.js
@@ -3,18 +3,30 @@ const Calculator = require('./Calculator');
 // Calculator testing add method
 describe('Calculator add method', () => {
     it('Add 2 + 5', () => {
+        // Arrange
         const calculator = new Calculator(2,5);
-        expect(calculator.add()).toBe(7);
+
+        // Act
+        const addMethod = calculator.add();
+
+        // Assert
+        expect(addMethod).toBe(7);
     })
 
     it('Add -8 + 50', () => {
         const calculator = new Calculator(-8,50);
-        expect(calculator.add()).toBe(42);
+
+        const addMethod = calculator.add();
+
+        expect(addMethod).toBe(42);
     })
 
     it('Add 22 + 32', () => {
         const calculator = new Calculator(22,32);
-        expect(calculator.add()).toBe(54);
+
+        const addMethod = calculator.add();
+
+        expect(addMethod).toBe(54);
     })
 })
 
@@ -22,35 +34,54 @@ describe('Calculator add method', () => {
 describe('Calculator subtract method', () => {
     it('subtract 87 - 52', () => {
         const calculator = new Calculator(87,-52);
-        expect(calculator.subtract()).toBe(139);
+
+        const subtractMethod = calculator.subtract();
+
+        expect(subtractMethod).toBe(139);
     })
 
     it('subtract -8 - 568', () => {
         const calculator = new Calculator(-8,568);
-        expect(calculator.subtract()).toBe(-576);
+
+        const subtractMethod = calculator.subtract();
+
+        expect(subtractMethod).toBe(-576);
     })
 
     it('subtract 22 - 32', () => {
         const calculator = new Calculator(22,32);
-        expect(calculator.subtract()).toBe(-10);
+
+        const subtractMethod = calculator.subtract();
+        
+        expect(subtractMethod).toBe(-10);
     })
 })
 
 // Calculator divide add method
 describe('Calculator divide method', () => {
-    //Failure message: Math error: division by zero
 
     it('divide 2 / 5', () => {
         const calculator = new Calculator(2,5);
-        expect(calculator.divide()).toBe(0.4);
+
+        const divideMethod = calculator.divide();
+
+        expect(divideMethod).toBe(0.4);
     })
+
     it('divide 0 / 5000', () => {
         const calculator = new Calculator(0,5000);
-        expect(calculator.divide()).toBe(Infinity);
+
+        const divideMethod = calculator.divide();
+
+        expect(divideMethod).toBe(Infinity);
     })
+
     it('divide 22 / 0', () => {
         const calculator = new Calculator(22,0);
-        expect(calculator.divide()).toBe(Infinity);
+
+        const divideMethod = calculator.divide();
+        
+        expect(divideMethod).toBe(Infinity);
     })
 })
 
@@ -58,14 +89,23 @@ describe('Calculator divide method', () => {
 describe('Calculator multiply method', () => {
     it('multiply 2 * 5', () => {
         const calculator = new Calculator(2,5);
-        expect(calculator.multiply()).toBe(10);
+
+        const multiplyMethod = calculator.multiply();
+
+        expect(multiplyMethod).toBe(10);
     })
     it('multiply -8 * 50', () => {
         const calculator = new Calculator(-8,50);
-        expect(calculator.multiply()).toBe(-400);
+
+        const multiplyMethod = calculator.multiply();
+
+        expect(multiplyMethod).toBe(-400);
     })
     it('multiply 22 * 0', () => {
         const calculator = new Calculator(22,0);
-        expect(calculator.multiply()).toBe(0);
+
+        const multiplyMethod = calculator.multiply();
+        
+        expect(multiplyMethod).toBe(0);
     })
 })
